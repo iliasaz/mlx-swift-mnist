cebba9de9ec2a7a05e576d8569a774454960a4d2
Ilia Sazonov <ilia_saz@yahoo.com>
2024-11-11 15:53:30 -0800

    Initial Commit

---
+++ mlx-swift-mnist/mlx_swift_mnistApp.swift
@@ -0,0 +1,17 @@
+//
+//  mlx_swift_mnistApp.swift
+//  mlx-swift-mnist
+//
+//  Created by Ilia Sazonov on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct mlx_swift_mnistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
